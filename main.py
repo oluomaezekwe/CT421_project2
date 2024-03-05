489bce2f0f4dd2e31d345b65a2b373a5b3b315c2
Irene Tressa Boby <i.boby1@universityofgalway.ie>
2024-03-05 17:15:06 +0000

    Added graph labelling

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import networkx as nx
 import matplotlib.pyplot as plt
-
+import random
 
 def create_2d_lattice_graph(a, b):
     graph = nx.Graph()
@@ -20,13 +20,19 @@ def create_2d_lattice_graph(a, b):
 
     return graph
 
+def label_graph(graph):
+    labels = {}
+    for node in graph.nodes():
+        labels[node] = random.randint(1, 5)  # Assigning random labels
+    return labels
 
-m = 2
-n = 2
+m = 5
+n = 5
 G = create_2d_lattice_graph(m, n)
+labels = label_graph(G)
 
 print("Nodes:", G.nodes())
 print("Edges:", G.edges())
 
-nx.draw(G)
+nx.draw(G, labels=labels, with_labels=True)
 plt.show()
