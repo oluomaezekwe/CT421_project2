991e1067e859c9c272bc0b7424d43c9d9fe88c44
Oluoma Ezekwe <o.ezekwe1@universityofgalway.ie>
2024-03-05 17:53:59 +0000

    add function to count the number of conflicts in a graph

--- main.py
+++ main.py
@@ -2,37 +2,53 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import random
 
-def create_2d_lattice_graph(a, b):
+
+def create_2d_lattice_graph(nodes, edges):
     graph = nx.Graph()
 
-    # Add nodes
-    for i in range(a):
-        for j in range(b):
+    for i in range(nodes):
+        for j in range(edges):
             graph.add_node((i, j))
 
-    # Add edges
-    for i in range(a):
-        for j in range(b):
-            if i < a - 1:
+    for i in range(nodes):
+        for j in range(edges):
+            if i < nodes - 1:
                 graph.add_edge((i, j), (i + 1, j))
-            if j < b - 1:
+            if j < edges - 1:
                 graph.add_edge((i, j), (i, j + 1))
 
     return graph
 
-def label_graph(graph):
+
+def label_graph(graph, min_val, max_val):
     labels = {}
+
     for node in graph.nodes():
-        labels[node] = random.randint(1, 5)  # Assigning random labels
+        labels[node] = random.randint(min_val, max_val)
     return labels
 
-m = 5
-n = 5
-G = create_2d_lattice_graph(m, n)
-labels = label_graph(G)
 
-print("Nodes:", G.nodes())
-print("Edges:", G.edges())
+def count_conflicts(graph, labels):
+    num_conflicts = 0
+
+    for node in graph.nodes():
+        neighbors = graph.neighbors(node)
+        for neighbor in neighbors:
+            if labels[node] == labels[neighbor]:
+                num_conflicts += 1
+    return num_conflicts
+
+
+m = 8
+n = 8
+p = 1
+q = 25
+
+G = create_2d_lattice_graph(m, n)
+labels = label_graph(G, p, q)
+
+conflicts = count_conflicts(G, labels)
+print("Number of Conflicts:", conflicts)
 
 nx.draw(G, labels=labels, with_labels=True)
 plt.show()
